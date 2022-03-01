b6884d2f3628c782977f53f16bfd89a3dcbf9686
Nico Gallardo <alfredongallg@gmail.com>
2022-03-01 01:16:53 -0300

    Se efectuan cambios en rutas, vistas y controladores de productos

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -1,6 +1,7 @@
+const { redirect } = require("express/lib/response");
 const fs = require("fs");
 
-const productos = JSON.parse(fs.readFileSync("../data/products.json"));
+const productos = JSON.parse(fs.readFileSync("../data/products.json","utf-8"));
 
 const controlador= {
     
@@ -24,6 +25,30 @@ const controlador= {
         res.render("creacionproducto");
     },
 
+    registrar:(req,res)=>{
+        let nuevoProducto = {
+            id: productos.length+1,
+            nombre:req.body.nombre ,
+            descripcion:req.body.descripcion ,
+            talles:req.body.talles,
+            composicion: req.body.composicion,
+            categoria:req.body.categoria ,
+            precio: req.body.precio,
+            imagen:"/images/productos/" + req.file.filename,
+        }
+
+        productos.push(nuevoProducto);
+
+        arrayModificado = productos;
+
+        //Añadir el nuevo producto al archivo JSON
+
+        fs.writeFileSync("../data/products.json",JSON.stringify(arrayModificado));
+
+        res.redirect("/producto");
+
+    },
+
     editar:(req, res) => {
         let idProducto =req.params.idProducto;
         var producto;
@@ -35,7 +60,15 @@ const controlador= {
         res.render('editarprod',{'producto':producto});
     },
 
-
+    borrar: (req,res)=>{
+        let idProducto =req.params.idProducto;
+        for(let i=0;i<productos.length;i++){
+            if (productos[i].id==idProducto){
+                productos.splice(i)
+            }
+        }
+        res.redirect("/");
+    }
 };
 
 module.exports=controlador;
--- src/routes/producto.js
+++ src/routes/producto.js
@@ -1,14 +1,51 @@
 const express=require("express");
 const router = express.Router();
-
+const {body} = require("express-validator");
+const multer = require("multer");
 const productoController = require('../controllers/productoController');
+const path = require('path');
 
+//validaciones
+const validacionProducto=[
+    body('nombre').notEmpty(),
+    body('descripcion').notEmpty(),
+    body('categoria').notEmpty(),
+    body('talles').notEmpty(),
+]
+
+//Config de multer
+let multerDiskStorage = multer.diskStorage({
+    destination:(req,file,callback)=>{
+        let folder = path.join(__dirname,'../../public/images/productos');
+        callback(null,folder);
+    },
+    filename:(req,file,callback)=>{
+        let imageName = Date.now() + path.extname(file.originalname);
+        callback(null,imageName);
+    }
+});
+
+let fileUpload = multer({storage: multerDiskStorage});
+
+//Muestra todos los productos
 router.get("/",productoController.catalogo);
 
+//Detalle del producto
 router.get("/detalle/:idProducto",productoController.detalle);
 
+//Formulario de creación de producto
 router.get("/create",productoController.crear);
 
+//Procesamiento formulario de creación
+router.post("/create",fileUpload.single('img'), productoController.registrar);
+
+//Formulario de edición de producto
 router.get("/edit/:idProducto",productoController.editar);
+
+//Procesamiento formulario de edición
+router.put("/edit/:idProducto",fileUpload.single('img'),productoController.editar);
+
+//Borrado de producto
+router.delete("/delete/:idProducto",productoController.borrar);
 
 module.exports=router;
