0ac3faec3d4c7e1add3966c28d9feece7908bc3e
Nico Gallardo <alfredongallg@gmail.com>
2022-05-18 18:07:24 -0300

    Se corrige error Multer

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express=require("express");
 
 const router = express.Router();
 
-const fileUpload = require('../../middlewares/multerMiddleware');
+const fileUpload = require('../../middlewares/multerUserMiddleware');
 
 const userController = require('../controllers/userController');
 
